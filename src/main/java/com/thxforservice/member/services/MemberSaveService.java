ccefa1bf867c88a791d797419d2ed2dc20652c2c
이민혁 <alsgur6624@gmail.com>
2024-09-25 10:43:47 +0900

    에러 체킹 콘솔로그

--- src/main/java/com/thxforservice/member/services/MemberSaveService.java
+++ src/main/java/com/thxforservice/member/services/MemberSaveService.java
@@ -77,6 +77,8 @@ public class MemberSaveService {
             employeeRepository.saveAndFlush(employee);
 
         } else if (member instanceof Student student){ // 학생 추가 정보
+            Employee employee = employeeRepository.findById(form.getProfessor()).orElse(null);
+            student.setProfessor(employee);
             student.setStudentNo(form.getStudentNo());
             student.setGrade(form.getGrade());
             student.setDepartment(form.getDepartment());
